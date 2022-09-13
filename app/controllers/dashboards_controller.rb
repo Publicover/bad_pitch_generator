1e466162396edf91fd08d8cfc148e2eb4b3f9c0d
publicover <jameslpublicover@gmail.com>
2022-09-13 11:43:25 +0200

    sentencer hooked up

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,3 +1,10 @@
 class DashboardsController < ApplicationController
-  def index; end
+  def index
+    @sentence = Sentencer.new.call
+
+    respond_to do |format|
+      format.html
+      format.js { render layout: false }
+    end
+  end
 end
