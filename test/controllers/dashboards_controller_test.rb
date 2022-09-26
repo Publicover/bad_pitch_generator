07d850e85963c070beeeae29cc15782cd003de49
publicover <jameslpublicover@gmail.com>
2022-09-27 00:22:10 +0200

    new pitch reveal

--- test/controllers/dashboards_controller_test.rb
+++ test/controllers/dashboards_controller_test.rb
@@ -1,6 +1,9 @@
 require "test_helper"
 
 class DashboardsControllerTest < ActionDispatch::IntegrationTest
+  longest_words = "We are a consulting company who brings structure to business
+                  as usual for customer-service-oriented cowboys and astronauts
+                  that want to brings structure to business as usual."
   test 'should get root without logging in' do
     get root_path
     assert_response :success
